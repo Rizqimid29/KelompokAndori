9eeba554080ed79c4a38411dd5024f9457f337bc
Ash <79562396+ashenarx@users.noreply.github.com>
2025-09-25 15:19:04 +0700

    login layout adjustments

--- app/src/main/java/com/example/kelompokandori/ui/auth/Login.kt
+++ app/src/main/java/com/example/kelompokandori/ui/auth/Login.kt
@@ -4,12 +4,20 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
 import com.example.kelompokandori.ui.home.Profile
 
 class Login : ComponentActivity() {
@@ -20,13 +28,27 @@ class Login : ComponentActivity() {
             var username by remember { mutableStateOf("") }
             var password by remember { mutableStateOf("") }
 
-            Column {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
                 TextField(value = username, onValueChange = { username = it }, placeholder = { Text("Username") })
-                TextField(value = password, onValueChange = { password = it }, placeholder = { Text("Password") })
+                TextField(
+                    value = password,
+                    onValueChange = { password = it },
+                    placeholder = { Text("Password") },
+                    visualTransformation = PasswordVisualTransformation()
+                )
                 Button(onClick = {
                     context.startActivity(Intent(context, Profile::class.java))
                 }) {
                     Text("Login")
+                }
+                TextButton(onClick = { }) {
+                    Text("Lupa Password")
                 }
                 Button(onClick = {
                     context.startActivity(Intent(context, Register::class.java))
